fefdf48757577462f07873b52d3a57b163069c2e
Seas0 <season1277@outlook.com>
2022-09-27 23:09:45 +0800

    [Kernel]: Implement user_va_to_pa; C4.1:LAB2.1 Cplt

--- kernel/vmm.c
+++ kernel/vmm.c
@@ -159,8 +159,10 @@ void *user_va_to_pa(pagetable_t page_dir, void *va) {
   // (va & (1<<PGSHIFT -1)) means computing the offset of "va" inside its page.
   // Also, it is possible that "va" is not mapped at all. in such case, we can find
   // invalid PTE, and should return NULL.
-  panic( "You have to implement user_va_to_pa (convert user va to pa) to print messages in lab2_1.\n" );
+  // panic( "You have to implement user_va_to_pa (convert user va to pa) to print messages in lab2_1.\n" );
 
+  uint64 pa_base = lookup_pa(page_dir, (uint64)va);
+  return pa_base ? (void*)(pa_base + ((uint64)va & ((1u << PGSHIFT) - 1))) : NULL;
 }
 
 //
